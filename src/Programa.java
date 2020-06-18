37c4522ce18aaadcc4979230ccb0a72b6fbee78b
Carol <51242985+carolquiterio@users.noreply.github.com>
2020-06-18 18:36:51 -0300

    Console changes

--- src/Programa.java
+++ src/Programa.java
@@ -7,7 +7,6 @@ public class Programa
         try
         {
 			Scanner in = new Scanner(System.in);
-        	//ListaSimplesDesordenada<Aeroporto> lis = new ListaSimplesDesordenada<Aeroporto> ();
         	ListaSimplesOrdenada<Aeroporto> lisOrd = new ListaSimplesOrdenada<Aeroporto>();
         	int entrada = 0;
         	int vez = 0;
@@ -25,21 +24,14 @@ public class Programa
 				for(int count=0 ; count<50 ; count++)
 				    System.out.println("");
 
-
-
 				if (entrada == 1)
 				{
 					vez = vez + 1;
 
-					System.out.println("Digite o numero, o código e o nome do aeroporto!");
-					// numero = in.nextInt();
+					System.out.println("Digite o código e o nome do aeroporto!");
 					String cod = in.next();
 					String nom = in.next();
-					//if(lis.tem(lis.getComParam(numero)));
-					//lis.insiraNoInicio(new Aeroporto(cod, nom, vez));
-					//if(lisOrd.tem(lisOrd.getComParam(vez)));
-					Aeroporto aero;
-					lisOrd.insira(aero = new Aeroporto(cod, nom, vez));
+					lisOrd.insira(new Aeroporto(cod, nom, vez));
 					System.out.println("Aeroporto cadastrado!");
 				}
 
@@ -49,13 +41,7 @@ public class Programa
 					int numeroOrigem = in.nextInt();
 					int numeroDestino = in.nextInt();
 					int numVoo = in.nextInt();
-
-			//		if(!lis.tem(lis.getComParam(numeroDestino)))
-			//					System.out.println("Insira uma cidade válida!");
-
-			//		lis.getComParam(numeroOrigem).guardeUmVoo(new Voo(numeroDestino, numVoo));
-
-					lisOrd.getComParam(numeroOrigem).guardeUmVoo(new Voo (numeroDestino, numVoo));
+					lisOrd.getComParam(numeroOrigem).guardeUmVoo(new Voo (numVoo, numeroDestino));
 					System.out.println("Voo cadastrado!");
 				}
 
@@ -64,28 +50,26 @@ public class Programa
 					System.out.println("Digite o número do aeroporto e o número do voo que deseja remover!");
 					int numeroAero = in.nextInt();
 					int numeroVoo = in.nextInt();
-			//		lis.getComParam(numeroAero).removaUmVoo(lis.getComParam(numeroAero).getVooComParam(numeroVoo));
 					lisOrd.getComParam(numeroAero).removaUmVoo(lisOrd.getComParam(numeroAero).getVooComParam(numeroVoo));
 					System.out.println("Voo removido!");
 				}
 
 				if(entrada == 4)
 				{
-					System.out.println("Digite o número no aeroporto que deseja consultar!");
+					System.out.println("Digite o número do aeroporto que deseja consultar!");
 					int num = in.nextInt();
-					//System.out.println(lis.getComParam(num).listaVoos());
+					System.out.println(lisOrd.getComParam(num).listaVoos());
 				}
 
 				if (entrada == 5)
 				{
-			//		int qtd = lis.getQtd();
 					int qtd = lisOrd.getQtd();
 					for (int i = 1; i <= qtd; i++)
 					{
-						System.out.println(lisOrd.getComParam(i));
+						System.out.println(lisOrd.getComParam(i).getCodigo() + ":" );
+						System.out.println(lisOrd.getComParam(i).listaVoos());
 					}
 				}
-
 			}
 			while (entrada!=999);
         }
